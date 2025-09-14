ef14403f7f7852ca73888c059932f598340e62b3
maria.fatu <maria.fatu@devops.ncia.nato.int>
2025-09-14 17:53:43 +0200

    feat: Add swagger ui

---
+++ src/ClamJam.Api/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+using ClamJam.Domain.Entities;
+using CleanJam.Application.Cart;
+using CleanJam.Application.Discount;
+using CleanJam.Application.Export;
+using CleanJam.Application.Pricing;
+using CleanJam.Application.Product;
+using CleanJam.Application.Tax;
+using CleanJam.Infrastructure;
+using CleanJam.Infrastructure.Export;
+using CleanJam.Infrastructure.Repository;
+
+namespace ClamJam.Api.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddClamJamServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configuration
+        services.AddSingleton<IProductRepository, InMemoryProductRepository>();
+
+        return services;
+    }
+}
--- src/ClamJam.Api/Program.cs
+++ src/ClamJam.Api/Program.cs
@@ -1,8 +1,31 @@
+﻿using ClamJam.Api.Configuration;
 using ClamJam.Library;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add ClamJam services
+builder.Services.AddClamJamServices(builder.Configuration);
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClamJam");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapControllers();
+
 
 var shop = new ShopManager();
 
